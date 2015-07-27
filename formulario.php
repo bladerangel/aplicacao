f3fcd03c16307878c358b837fe1ea996d502ae6d
Luizangel50 <luiz.angel.100@gmail.com>
2015-07-27 01:41:24 -0300

    Sixth

--- formulario.php
+++ formulario.php
@@ -1,19 +1,49 @@
+<?
+
+	function preencher($campo)
+	{
+		if(isset($_POST[$campo]))
+			if($campo != "sexo")
+				echo "value='" .$_POST[$campo]."'";
+			else
+				{
+					if($_POST[$campo] != null)
+						echo "checked";
+					// else
+					// 	echo "checked";
+				}
+	}
+?>
+
+
+
+
+
 <html>
-<head>
-	<title>Formulario</title>
-</head>
-<body>
-<form action="formulario.php" method="POST">
-Nome:<br><input type="text" name="nome" <? if(isset($_POST["nome"])) echo "value='" .$_POST["nome"]."'"?>>
-<br>
-Idade:<br><input type="text" name="idade">
-<br>
-Sexo:<br>
-<input type="radio" name="sexo" value="masculino">Masculino
-<br>
-<input type="radio" name="sexo" value="feminino">Feminino
-<br><br>
-<input type="submit" value="Submit">
-</form>
-</body>
+
+	<head>
+
+		<title>Formulario</title>
+
+	</head>
+
+	<body>
+
+		<form action="formulario.php" method="POST">
+
+			Nome:<br><input type="text" name="nome" <? preencher("nome"); ?>>
+			<br>
+			Idade:<br><input type="text" name="idade" <? preencher("idade"); ?>>
+			<br>
+			Sexo:<br>
+			<input type="radio" name="sexo" value="masculino" <? preencher("sexo"); ?>>Masculino
+			<br>
+			<input type="radio" name="sexo" value="feminino" <? preencher("sexo"); ?>>Feminino
+			<br><br>
+			<input type="submit" value="Submit">
+
+		</form>
+
+	</body>
+
 </html>
